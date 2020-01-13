f3a8db86e9958d0b757e6dfeec1c54befff32fb0
Dalapi <38732983+Dongbei-Dalapi@users.noreply.github.com>
2020-01-13 22:17:17 +0100

    Update edge_detector.py

--- src/edge_detector.py
+++ src/edge_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-import argparse, os
+import argparse
+from ast import literal_eval
 
 
 def noise_filter(image, mode=None, kernel=None):
@@ -27,6 +28,10 @@ def noise_filter(image, mode=None, kernel=None):
 
 
 def edge_detector(image, method='canny'):
+    """ edge detection
+            image     a image stored in ndarray
+            method      the different way of the edge detection('Laplace', 'Sobel' and 'Canny')
+    """
     if method.lower() == 'laplacian' or 'laplace':
         lap = cv2.Laplacian(image, cv2.CV_64F)
         lap = np.uint8(np.absolute(lap))
@@ -41,48 +46,65 @@ def edge_detector(image, method='canny'):
     elif method.lower() == 'canny':
         edges = cv2.Canny(image, 100, 200)
         return edges
+    else:
+        print('no edge detector specified')
 
 
 if __name__ == '__main__':
+
     # input image as the argument
     argp = argparse.ArgumentParser()
     argp.add_argument('image')
+    argp.add_argument('--filter', default='bilateral')
+    argp.add_argument('--kernel', default=None)
+    argp.add_argument('--detector', default='canny')
     args = argp.parse_args()
 
     # load the image
-    filename = args.image
     img_original = cv2.imread(args.image)
-
     # convert the image into gray scale
     img_gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
 
     # noise reduction
-    filtered_image = noise_filter(img_gray, mode='bilateral')
-    filtered_image = noise_filter(filtered_image, mode='bilateral')
+    kernel = None
+    if args.kernel:
+        k = literal_eval(args.kernel)
+        kernel = np.array(k, dtype="float32")
+    filtered_image = noise_filter(img_gray, mode=args.filter, kernel=kernel)
 
-    # use thresshold to convert image into black and white
-    _, th = cv2.threshold(filtered_image, 150, 255, cv2.THRESH_BINARY)
-    cv2.imshow('th', th)
+    # use threshold to convert image into black and white
+    _, th = cv2.threshold(filtered_image, 170, 255, cv2.THRESH_BINARY)
 
     # edge detection
-    edges = edge_detector(th, 'canny')
-    cv2.imshow('e',edges)
+    edges = edge_detector(th, method=args.detector)
+
+    # lines
+    l = img_original.copy()
+    minLineLength = 70
+    maxLineGap = 2
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(l, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    cv2.imwrite('houghlines5.jpg', l)
 
     # draw edges
     for i in range(edges.shape[0]):
         for j in range(edges.shape[1]):
-            if edges[i][j] >= 127:
+            if edges[i][j] >= 70:
                 img_original[i][j][0] = 0
                 img_original[i][j][1] = 255
                 img_original[i][j][2] = 0
 
+    # create a new filename to output the image
+    filename = args.image
     filename = filename[8:]
     new_filename = '../output/' + filename[:filename.index('.')]
     file_type = filename[filename.index('.'):]
     whole_filename = new_filename + '_edges' + file_type
 
     cv2.imwrite(whole_filename, img_original)
-    #cv2.imshow('result', img_original)
 
-    cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
